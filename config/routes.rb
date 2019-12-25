7902a4437ccd42a777ace0e0c0e80ff7989a11a8
ahmedalhindi <alhindi.ahmed@gmail.com>
2019-12-25 11:50:46 +0300

    installed devise with manger model and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :managers
+  root to: "ques#index"
+  
   resources :ques do
     resources :services
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Que.create(name: "que_1", email: "email_1", location: "location_1", img: "img_1")
-Que.create(name: "que_2", email: "email_2", location: "location_2", img: "img_2")
-Que.create(name: "que_3", email: "email_3", location: "location_3", img: "img_3")
-Que.create(name: "que_4", email: "email_4", location: "location_4", img: "img_4")
-Que.create(name: "que_5", email: "email_5", location: "location_5", img: "img_5")
+# Que.create(name: "que_1", email: "email_1", location: "location_1", img: "img_1")
+# Que.create(name: "que_2", email: "email_2", location: "location_2", img: "img_2")
+# Que.create(name: "que_3", email: "email_3", location: "location_3", img: "img_3")
+# Que.create(name: "que_4", email: "email_4", location: "location_4", img: "img_4")
+# Que.create(name: "que_5", email: "email_5", location: "location_5", img: "img_5")
 
-# Seeding services for Que 1
-Que.find(1).services.create(name: "Service_1", description: "Description_1")
-Que.find(1).services.create(name: "Service_2", description: "Description_2")
-Que.find(1).services.create(name: "Service_3", description: "Description_3")
-Que.find(1).services.create(name: "Service_4", description: "Description_4")
+# # Seeding services for Que 1
+# Que.find(1).services.create(name: "Service_1", description: "Description_1")
+# Que.find(1).services.create(name: "Service_2", description: "Description_2")
+# Que.find(1).services.create(name: "Service_3", description: "Description_3")
+# Que.find(1).services.create(name: "Service_4", description: "Description_4")
 
-# Seeding services for Que 2
-Que.find(2).services.create(name: "Service_1", description: "Description_1")
-Que.find(2).services.create(name: "Service_2", description: "Description_2")
+# # Seeding services for Que 2
+# Que.find(2).services.create(name: "Service_1", description: "Description_1")
+# Que.find(2).services.create(name: "Service_2", description: "Description_2")
 
 
